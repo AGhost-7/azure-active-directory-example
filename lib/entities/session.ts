f2d340c5733e5c6df6a2bcf6f71fb625e9f15837
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-07-28 19:45:16 -0400

    format code

--- lib/entities/session.ts
+++ lib/entities/session.ts
@@ -1,27 +1,26 @@
-
-import {Entity, Column, PrimaryColumn} from 'typeorm'
+import { Entity, Column, PrimaryColumn } from 'typeorm'
 
 @Entity()
 class Session {
-	@PrimaryColumn()
-	key: string
-	@Column('json')
-	session: object
-	@Column()
-	expiresAt: Date
-	@Column()
-	createdAt: Date
+  @PrimaryColumn()
+  key: string
+  @Column('json')
+  session: object
+  @Column()
+  expiresAt: Date
+  @Column()
+  createdAt: Date
 
-	constructor(key: string, session: object, maxAge: number) {
-		this.key = key
-		this.createdAt = new Date()
-		this.expiresAt = new Date(this.createdAt.getTime() + maxAge)
-		this.session = session
-	}
+  constructor(key: string, session: object, maxAge: number) {
+    this.key = key
+    this.createdAt = new Date()
+    this.expiresAt = new Date(this.createdAt.getTime() + maxAge)
+    this.session = session
+  }
 
-	set maxAge(value: number) {
-		this.expiresAt = new Date(Date.now() + value)
-	}
+  set maxAge(value: number) {
+    this.expiresAt = new Date(Date.now() + value)
+  }
 }
 
 export default Session
